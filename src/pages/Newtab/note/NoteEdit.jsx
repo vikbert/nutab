355e23022ca8b132b44621c1e51dfd0b12307025
Xun Zhou <segentor@gmail.com>
2020-05-21 21:23:32 +0200

    updates

--- src/pages/Newtab/note/NoteEdit.jsx
+++ src/pages/Newtab/note/NoteEdit.jsx
@@ -4,11 +4,12 @@ import noteStore from "../../../storages/NoteStore";
 const NoteEdit = ({note}) => {
     const textRef = useRef();
     const [content, setContent] = useState(note.content || '');
+    const [title, setTitle] = useState(note.title || (new Date()).toLocaleDateString());
     const [textRows, setTextRows] = useState(3);
 
     const handleSubmitNote = (event) => {
         if (content.length) {
-            noteStore.update({...note, content: content});
+            noteStore.update({...note, content: content, title: title});
         }
     };
 
@@ -22,14 +23,24 @@ const NoteEdit = ({note}) => {
     }, []);
 
     return (
-        <textarea
-            className={'textarea-note'}
-            ref={textRef}
-            value={content}
-            onChange={handleChangeText}
-            onBlur={handleSubmitNote}
-            rows={textRows}
-        />
+        <>
+            <input
+                className={'note-input'}
+                type="text" 
+                value={title} 
+                placeholder={'Title'}
+                onChange={(event) => setTitle(event.target.value)}
+                onBlur={handleSubmitNote}
+            />
+            <textarea
+                className={'note-textarea'}
+                ref={textRef}
+                value={content}
+                onChange={handleChangeText}
+                onBlur={handleSubmitNote}
+                rows={textRows}
+            />
+        </>
     );
 };
 

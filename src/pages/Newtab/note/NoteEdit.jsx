dfbe2e63f66bf32f8600db9b2d1a3f1c4efa059a
vikbert <segentor@gmail.com>
2020-06-06 07:34:50 +0200

    updates

--- src/pages/Newtab/note/NoteEdit.jsx
+++ src/pages/Newtab/note/NoteEdit.jsx
@@ -1,47 +1,48 @@
-import React, {useRef, useState, useEffect} from 'react';
-import noteStore from "../../../storages/NoteStore";
+import React, { useRef, useState, useEffect } from 'react';
+import noteStore from '../../../storages/NoteStore';
 
-const NoteEdit = ({note}) => {
-    const textRef = useRef();
-    const [content, setContent] = useState(note.content || '');
-    const [title, setTitle] = useState(note.title || '');
-    const [textRows, setTextRows] = useState(3);
+const NoteEdit = ({ note }) => {
+  const textRef = useRef();
+  const [content, setContent] = useState(note.content || '');
+  const [title, setTitle] = useState(note.title || '');
+  const [textRows, setTextRows] = useState(1);
 
-    const handleSubmitNote = (event) => {
-        if (content.length) {
-            noteStore.update({...note, content: content, title: title});
-        }
-    };
+  const handleSubmitNote = (event) => {
+    noteStore.update({ ...note, content: content, title: title });
+  };
 
-    const handleChangeText = (event) => {
-        setContent(event.target.value);
-    };
+  const handleChangeText = (event) => {
+    setContent(event.target.value);
+  };
 
-    useEffect(() => {
-        const rowsOfTextArea = Math.floor(textRef.current.scrollHeight / 20);
-        setTextRows(rowsOfTextArea);
-    }, []);
+  useEffect(() => {
+    if (content.trim().length > 0) {
+      const rowsOfTextArea = Math.floor(textRef.current.scrollHeight / 20);
+      setTextRows(rowsOfTextArea);
+    }
+  }, []);
 
-    return (
-        <>
-            <input
-                className={'note-input'}
-                type="text" 
-                value={title} 
-                placeholder={'Title'}
-                onChange={(event) => setTitle(event.target.value)}
-                onBlur={handleSubmitNote}
-            />
-            <textarea
-                className={'note-textarea'}
-                ref={textRef}
-                value={content}
-                onChange={handleChangeText}
-                onBlur={handleSubmitNote}
-                rows={textRows}
-            />
-        </>
-    );
+  return (
+    <>
+      <input
+        className={'note-input'}
+        type="text"
+        value={title}
+        placeholder={'Title'}
+        onChange={(event) => setTitle(event.target.value)}
+        onBlur={handleSubmitNote}
+      />
+
+      <textarea
+        className={'note-textarea'}
+        ref={textRef}
+        value={content}
+        onChange={handleChangeText}
+        onBlur={handleSubmitNote}
+        rows={textRows}
+      />
+    </>
+  );
 };
 
 export default NoteEdit;

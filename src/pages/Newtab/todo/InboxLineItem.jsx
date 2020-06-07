9cb47e63d9472d7997708f243e37808dd7c5161e
Xun Zhou <segentor@gmail.com>
2020-06-07 19:25:33 +0200

    inbox line item

---
+++ src/pages/Newtab/todo/InboxLineItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function InboxLineItem() {
+  return (
+    <li className={'category category-inbox'}>
+      <label>
+        <span className="icon icon-drawer2" style={{ paddingLeft: 0 }} />
+      </label>
+    </li>
+  );
+}

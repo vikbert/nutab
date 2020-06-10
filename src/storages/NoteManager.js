72e6cc8068209bd49e42a0ce9689ab672121905e
Xun Zhou <segentor@gmail.com>
2020-06-11 00:00:55 +0200

    updates

---
+++ src/storages/NoteManager.js
@@ -0,0 +1,150 @@
+class NoteManager {
+  constructor(noteData) {
+    if (!noteData.hasOwnProperty('notes')) {
+      throw new Error('note data has no propery: [notes]');
+    }
+
+    if (!noteData.notes.hasOwnProperty('byId')) {
+      throw new Error('note data has no propery: [byId]');
+    }
+
+    if (!noteData.notes.hasOwnProperty('bookmarked')) {
+      throw new Error('note data has no propery: [bookmarked]');
+    }
+
+    if (!noteData.notes.hasOwnProperty('all')) {
+      throw new Error('note data has no propery: [all]');
+    }
+
+    this.data = JSON.parse(JSON.stringify(noteData));
+  }
+
+  countBookmarked() {
+    return this.data.notes.bookmarked.length;
+  }
+
+  countAll() {
+    return this.data.notes.all.length;
+  }
+
+  getNotes() {
+    return this.data.notes;
+  }
+
+  getNote(id) {
+    return this.data.notes.byId[id];
+  }
+
+  _updateNote(note) {
+    this.data.notes.byId[note.id] = note;
+  }
+
+  updateNoteContent(id, title, content) {
+    const previousNote = this.getNote(id);
+    this._updateNote({
+      ...previousNote,
+      title,
+      content,
+    });
+  }
+
+  _removeIdFromAll(id) {
+    const all = this.data.notes.all;
+    this.data.notes.all = all.filter((key) => key.toString() !== id.toString());
+  }
+
+  _removeIdFromBookmarked(id) {
+    const bookmarked = this.data.notes.bookmarked;
+    this.data.notes.bookmarked = bookmarked.filter((key) => key.toString() !== id.toString());
+  }
+
+  _addIdToAll(id) {
+    const all = this.data.notes.all;
+    this.data.notes.all = [id, ...all];
+  }
+
+  _addIdToBookmarked(id) {
+    const bookmarked = this.data.notes.bookmarked;
+    this.data.notes.bookmarked = [id, ...bookmarked];
+  }
+
+  toggleNoteBookmarked(id) {
+    const note = this.getNote(id);
+    if (!note) {
+      throw new Error(`Target Note not found: ${id}`);
+    }
+
+    this._updateNote({
+      ...note,
+      bookmarked: !note.bookmarked,
+    });
+
+    if (note.bookmarked) {
+      this._removeIdFromBookmarked(id);
+    } else {
+      this._addIdToBookmarked(id);
+    }
+  }
+
+  deleteNote(id) {
+    delete this.data.notes.byId[id];
+
+    this._removeIdFromAll(id);
+    this._removeIdFromBookmarked(id);
+  }
+
+  addNote(note) {
+    // add note to byId {}
+    const idString = note.id.toString();
+    this.data.notes.byId[idString] = note;
+
+    // add ID to all[]
+    this._addIdToAll(idString);
+
+    // if note.bookmarked:true, add ID to bookmarked[]
+    if (note.bookmarked) {
+      this._addIdToBookmarked(idString);
+    }
+  }
+
+  setBookmarked(ids) {
+    // be sure keys is an Array
+    if (!Array.isArray(ids)) {
+      throw new Error('Given Keys is not an Array!');
+    }
+
+    const notes = this.data.notes;
+    const existingBookmarked = this.data.notes.all.filter((key) => notes.byId[key].bookmarked);
+    if (existingBookmarked.sort().toString() !== [...ids].sort().toString()) {
+      throw new Error(
+        `IDs should not be changed, exclude their order. [${existingBookmarked.toString()}] vs [${ids.toString()}]`
+      );
+    }
+
+    this.data.notes.bookmarked = ids;
+  }
+
+  setAll(ids) {
+    // be sure keys is an Array
+    if (!Array.isArray(ids)) {
+      throw new Error('Given Keys is not an Array!');
+    }
+
+    const existingAll = [...this.data.notes.all];
+    if (existingAll.sort().toString() !== [...ids].sort().toString()) {
+      throw new Error(`IDs should not be changed, exclude their order`);
+    }
+
+    this.data.notes.all = ids;
+  }
+
+  getBookmarked() {
+    return this.data.notes.bookmarked;
+  }
+
+  getAll() {
+    return this.data.notes.all;
+  }
+}
+
+export default NoteManager;
---
+++ src/storages/__tests__/noteStore.test.js
@@ -0,0 +1,126 @@
+import noteMock from './noteMock.json';
+import NoteManager from '../NoteManager';
+describe('NoteManager', () => {
+  let manager;
+  beforeEach(() => {
+    manager = new NoteManager(noteMock);
+  });
+
+  it('countAll() ', () => {
+    expect(manager.countAll()).toEqual(3);
+  });
+
+  it('countBookmarked() ', () => {
+    expect(manager.countBookmarked()).toEqual(2);
+  });
+
+  it('getNotes()', () => {
+    expect(typeof manager.getNotes()).toEqual('object');
+
+    expect(Object.keys(manager.getNotes())).toContain('byId');
+    expect(Object.keys(manager.getNotes())).toContain('bookmarked');
+    expect(Object.keys(manager.getNotes())).toContain('all');
+  });
+
+  it('getNote()', () => {
+    expect(manager.getNote('11').id).toEqual('11');
+  });
+
+  it('updateNoteContent()', () => {
+    const newNote = {
+      id: '11',
+      content: 'content 99',
+      title: 'title 11',
+      bookmarked: true,
+    };
+    manager.updateNoteContent('11', 'title 11', 'content 99');
+    expect(manager.getNote('11')).toEqual(newNote);
+  });
+
+  it('deleteNote()', () => {
+    manager.deleteNote('11');
+
+    // note is removed from byId
+    expect(manager.getNote('11')).toEqual(undefined);
+
+    // id is removed from all[]
+    expect(manager.countAll()).toEqual(2);
+
+    // id is remove from bookmarked[]
+    expect(manager.countBookmarked()).toEqual(1);
+  });
+
+  it('toggleNoteBookmarked(ids)', () => {
+    expect(manager.getNote('11').bookmarked).toBeTruthy();
+    expect(manager.countBookmarked()).toEqual(2);
+
+    // check bookmarked true => false
+    manager.toggleNoteBookmarked('11');
+    expect(manager.getNote('11').bookmarked).toBeFalsy();
+    expect(manager.countBookmarked()).toEqual(1);
+  });
+
+  it('toggleNoteBookmarked(id)', () => {
+    // check bookmarked false => true
+    manager.toggleNoteBookmarked('33');
+    expect(manager.countBookmarked()).toEqual(3);
+    expect(manager.countAll()).toEqual(3);
+    expect(manager.getNote('33').bookmarked).toEqual(true);
+  });
+
+  it('addNote(newNote)', () => {
+    const note44 = {
+      id: '44',
+      title: 'title 44',
+      content: 'content 44',
+      bookmarked: false,
+    };
+    manager.addNote(note44);
+    expect(manager.countBookmarked()).toEqual(2);
+    expect(manager.countAll()).toEqual(4);
+  });
+
+  it('addNote(newNote)', () => {
+    const note55 = {
+      id: '55',
+      title: 'title 55',
+      content: 'content 55',
+      bookmarked: true,
+    };
+    manager.addNote(note55);
+    expect(manager.countBookmarked()).toEqual(3);
+    expect(manager.countAll()).toEqual(4);
+  });
+
+  it('setAll(ids)', () => {
+    const ids = ['11', '22', '44'];
+    expect(() => {
+      manager.setAll(ids);
+    }).toThrow();
+  });
+
+  it('setAll(ids)', () => {
+    const ids = ['11', '33', '22'];
+    manager.setAll(ids);
+  });
+
+  it('setBookmarked(ids)', () => {
+    const ids = ['11', '44'];
+    expect(() => {
+      manager.setBookmarked(ids);
+    }).toThrow();
+  });
+
+  it('setBookmarked(ids)', () => {
+    const ids = ['11', '22'];
+    manager.setBookmarked(ids);
+  });
+
+  it('getAll', () => {
+    expect(manager.getAll().toString()).toEqual(['11', '22', '33'].toString());
+  });
+
+  it('getBookmarked', () => {
+    expect(manager.getBookmarked().toString()).toEqual(['22', '11'].toString());
+  });
+});
